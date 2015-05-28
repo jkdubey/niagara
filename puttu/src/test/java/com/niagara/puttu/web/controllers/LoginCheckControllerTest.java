420e7974831ea6b993d2cb57b4794c930fae3508
jitendra <jkdubey@gmail.com>
2015-05-29 03:43:31 +0530

    created missing folder of test/src
    changed junit version to 4.11

---
+++ puttu/src/test/java/com/niagara/puttu/web/controllers/LoginCheckControllerTest.java
@@ -0,0 +1,24 @@
+package com.niagara.puttu.web.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginCheckControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLoginCheck() {
+		assertTrue("Not yet implemented", true);;
+	}
+
+}
